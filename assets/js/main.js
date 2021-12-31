0c52e80715a38f41b1bc3190de6c9ba1e95618ae
NguyenDung2491996 <nguyenvandung24996@gmail.com>
2021-12-31 15:50:26 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const iconElement = document.querySelector('.fa-bars');
+const menuList = document.querySelector(' .header .header_bottom .menu .list_menu');
+const subMenuList = document.querySelector('.header_bottom .list_menu .list_item .sub_menu');
+const iconDownElement = document.querySelector('.toggle-subMenu');
+
+if (iconElement) {
+  iconElement.addEventListener('click', handleToggleMenu);
+}
+
+function handleToggleMenu() {
+  if (menuList) menuList.classList.toggle('active');
+}
+
+if (iconDownElement) {
+  iconDownElement.addEventListener('click', handleToggleSubMenu);
+}
+
+function handleToggleSubMenu() {
+  if (subMenuList) subMenuList.classList.toggle('show');
+}
